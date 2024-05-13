31f839351dc360499912f5370252ac69080d44db
Mazen-Monem <mazenmonem22@gmail.com>
2024-05-13 20:41:09 +0300

    Installation Script files

---
+++ Jenkins.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#======== Installing Java ========#
+sudo apt update
+sudo apt install fontconfig openjdk-17-jre -y
+
+#======== Installing Jenkins ======#
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+#========= Start Jenkins Service ======#
+sudo systemctl enable --now jenkins
+
+#========== Change Server Name ========#
+sudo hostnamectl hostname jenkins
